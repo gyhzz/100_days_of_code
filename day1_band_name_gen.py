2780f94ada2dcbafe0120cd858cb36e396e037f4
Yui Hao <gohyuihao_@hotmail.com>
2022-11-19 22:52:35 +0800

    Add Day 1

---
+++ day1_band_name_gen.py
@@ -0,0 +1,13 @@
+if __name__ == '__main__':
+	
+	print('''Welcome to the Band Name Generator.
+What's the name of the city you grew up in?''')
+
+	city = input()
+
+	print('''What's your pet's name?''')
+
+	pet = input()
+
+	print(f'Your band name could be {city} {pet}')
+
